83ebb64d418d8c606c194abd6cb57ea027985df8
Ubuntu <ubuntu@ip-172-31-24-76.eu-west-1.compute.internal>
2025-12-08 21:11:10 +0000

    added shell script

---
+++ chatur.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "chatur: adarniya sabhapati mahodaya"
+
+echo "chatur: aaj hum sab mil ke chamatkaar karenge"
+
+echo "rancho: ratta marega toh aisa hi hoga"
+
+echo "chatur: thuchuk thuchuk"
+
+echo "raju: rancho itna hi tujhe toh ek folder bananke dikhana"
+
+mkdir -p rancho
+
+echo "rancho:ye leh,bana diya,ro mt ab"
+
+echo "farhan: virus ne beizzati kar di,bola farhanitrate"
+
+echo "farhan: abba nhi manenge" >farhanitrate.txt
